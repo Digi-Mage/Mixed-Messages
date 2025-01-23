6cfb424fabd697d9f89b95b7a2f38cddb6444123
Fels <147960937+Digi-Mage@users.noreply.github.com>
2025-01-23 16:00:24 -0700

    "Formatting Complete"

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,9 +1,9 @@
 
-let subjectOne = ['My dog', 'My sister', 'My brother', 'My girlfriend', 'Your mom', 'The mayor', 'The President']
-let action = ['ran', 'danced', 'swayed', 'swirved', 'swung', 'meandered', 'swam', 'jumped', 'leapt', 'fell', 'slipped', 'crouched', 'laid', 'wept']
+let subjectOne = ['My fairy godmother', 'A grumpy old man', 'Mysterious jester', 'A dancing potato', 'A sentient tree', 'A curious cat', 'An adventurous astronaut','My dog', 'My sister', 'My brother', 'My girlfriend', 'Your mom', 'The mayor', 'The President']
+let action = ['ran', 'danced', 'swayed', 'swirved', 'swung', 'meandered', 'swam', 'jumped', 'leapt', 'fell', 'slipped', 'crouched', 'laid', 'wept', 'slid']
 let description = ['silently', 'swiftly', 'quickly', 'slowly', 'loudly', 'lovingly', 'angrily', 'sadly', 'quietly']
-let preposition = ['into', 'onto', 'under', 'over', 'around']
-let subjectTwo = ['space', 'a chair', 'a table', 'the background', 'your mom', 'a jail cell', 'the ring', 'the sky', 'a pit', 'a yaht', 'a ski lift']
+let preposition = ['into', 'onto', 'under', 'over', 'around', 'behind']
+let subjectTwo = ['space', 'a chair', 'a table', 'the moonlight', 'the background', 'your mom', 'a jail cell', 'the ring', 'the sky', 'a pit', 'a yaht', 'a ski lift']
 let punctuation = ['.', '?', '!']
 
 const selection = (arr) => {
@@ -14,7 +14,9 @@ const finalMessage = () => {
     document.getElementById("final").innerHTML =(`${selection(subjectOne)} ${selection(action)} ${selection(description)} ${selection(preposition)} ${selection(subjectTwo)}${selection(punctuation)}`)
 }
 
-
+const formMessage = () => {
+    document.getElementById("form-final").innerHTML =(`${selection(subjectOne)} ${selection(action)} ${selection(description)} ${selection(preposition)} ${selection(subjectTwo)}${selection(punctuation)}`)
+}
 
 
 
